b64f96e8b75f46328d8ca4c5a371f632efbf49bf
JESINTH ARNOLD <39983328+jesintharnold@users.noreply.github.com>
2020-02-21 10:42:49 +0530

    Add files via upload

---
+++ templates/pages/Page-2/script-num.py
@@ -0,0 +1,12 @@
+# import RPi.GPIO as GPIO
+# from time import sleep
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(26,GPIO.IN)
+#
+# try:
+#     while True:  # this will carry on until you hit CTRL+C
+#         if GPIO.input(26):
+#
+#
+# except KeyboardInterrupt:
+#     GPIO.cleanup()
